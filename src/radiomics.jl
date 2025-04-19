eb22e99696549107b9c89bd0f5e088650ac27109
Paolo Zaffino <p.zaffino@unicz.it>
2025-04-20 01:07:39 +0200

    Early skeleton

--- src/radiomics.jl
+++ src/radiomics.jl
@@ -1,5 +1,15 @@
 module radiomics
 
-using NIfTI
 
+function extract_features(img, mask)
+    println("Extracting...")
+
+    first_order_features(img, mask)
+
+end
+
+
+function first_order_features(img, mask)
+    println("Extracting first order features...")
+end
 
