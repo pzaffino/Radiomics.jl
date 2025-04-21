2d1bdd283de774ac4a19cc5015deb69da6c456eb
Paolo Zaffino <p.zaffino@unicz.it>
2025-04-21 23:08:20 +0200

    Improved first order computational cost by pre-extractiong voxel within roi

--- src/first_order_features.jl
+++ src/first_order_features.jl
@@ -7,9 +7,10 @@ function first_order_features(img, mask, verbose=false)
 
     # Some data can be useful for features extraction
     voxel_volume = img.header.pixdim[2] * img.header.pixdim[3] * img.header.pixdim[4]
+    roi_voxels = extract_roi_voxels(img, mask)
 
     # Energy
-    energy = get_energy(img, mask)
+    energy = get_energy(roi_voxels)
     if verbose
         println("  Energy = $energy")
     end
@@ -22,21 +23,34 @@ function first_order_features(img, mask, verbose=false)
 
 end
 
-function get_energy(img, mask, c=0)
-    energy = 0.0
+
+function extract_roi_voxels(img, mask)
+    roi_voxels = Vector{Float32}()
     for i in 1:size(img, 1)
         for j in 1:size(img, 2)
             for k in 1:size(img, 3)
                 if mask[i,j,k] == 1
-                    energy = energy + (img[i,j,k] + c)^2
+                    push!(roi_voxels, img[i,j,k])
                 end
             end
         end
     end
+    return roi_voxels
+end
+
+
+function get_energy(roi_voxels, c=0.0)
+    energy = 0.0
+    for i in 1:size(roi_voxels, 1)
+        energy = energy + (roi_voxels[i] + c)^2
+    end
     return energy
 end
+
 
 function get_total_energy(voxel_volume, energy)
     total_energy = voxel_volume * energy
     return total_energy 
 end
+
+
