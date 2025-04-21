df41264e2ce7323703eeb608d26f2d94e62f3d8a
Paolo Zaffino <p.zaffino@unicz.it>
2025-04-21 23:43:50 +0200

    Improve sanity check

--- src/Radiomics.jl
+++ src/Radiomics.jl
@@ -2,11 +2,17 @@ module Radiomics
 
 include("first_order_features.jl")
 
-function extract_features(img, mask, verbose=false)
+
+function extract_features(img, mask, binarize_mask = false, verbose=false)
     println("Extracting...")
 
     # Sanity check on inputs
     input_sanity_check(img, mask, verbose)
+
+    # Binarize mask
+    if binarize_mask == true
+        mask = run_mask_binarization(mask)
+    end
 
     # First order features
     first_order_features(img, mask, verbose)
@@ -19,8 +25,25 @@ function input_sanity_check(img, mask, verbose)
         println("Running input sanity check...")
     end
 
+    img_offset = [img.header.qoffset_x, img.header.qoffset_y, img.header.qoffset_z]
+    mask_offset = [mask.header.qoffset_x, mask.header.qoffset_y, mask.header.qoffset_z]
+
     @assert size(img) == size(mask) "img and mask have different size!"
+    @assert img.header.pixdim[2:4] == mask.header.pixdim[2:4] "img and mask have different voxel size!"
+    @assert img_offset == mask_offset "img and mask have different offset!"
+
 end
+
+
+function run_mask_binarization(mask)
+
+    mask = mask .- minimum(mask)
+    mask = mask ./ maximum(mask)
+
+    return mask
+
+end
+
 
 end
 
