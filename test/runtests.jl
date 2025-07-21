be51723fd28bacb1753397fb7b43aa0cf959c54d
Mohammadreza Javadi Namin <59024947+mohamad199877@users.noreply.github.com>
2025-07-21 12:20:02 +0200

    Shape3D pull request (#3)
    * shape3D feature extraction module added

--- test/runtests.jl
+++ test/runtests.jl
@@ -49,7 +49,6 @@ end
     ct_slice = ct.raw[:, :, slice_idx]
     mask_slice = mask.raw[:, :, slice_idx]
 
-
     radiomic_features = Radiomics.extract_radiomic_features(ct_slice, mask_slice, spacing; force_2d=true, force_2d_dimension=2, verbose = false)
 
     @test isapprox(radiomic_features["shape2d_elongation"]                        , 0.6524707f0; atol=1e-4)
@@ -61,5 +60,30 @@ end
     @test isapprox(radiomic_features["shape2d_perimeter_surface_ratio"]           , 0.08065f0; atol=1e-4)
     @test isapprox(radiomic_features["shape2d_pixel_surface"]                     , 26556.75f0; atol=1e-4)
     @test isapprox(radiomic_features["shape2d_sphericity"]                        , 0.269386f0; atol=1e-4)
+end
 
-    end
+@testset "Radiomics test - Shape3D Features" begin
+    # Read NIfTI files
+
+    ct = niread("../sample_data/CTChest.nii.gz")
+    mask = niread("../sample_data/Lungs.nii.gz")
+    spacing = [ct.header.pixdim[2], ct.header.pixdim[3], ct.header.pixdim[4]]
+    spacing_perm = reverse(spacing)
+
+    mask_bin = mask.raw .> 0
+    mask_cc = Radiomics.keep_largest_component(mask_bin)
+
+    mask_pad = Radiomics.pad_mask(mask_cc, 1)
+    features = Radiomics.get_shape3d_features(mask_pad, spacing; verbose=true)
+
+    @test isapprox(features["shape3d_surface_area"], 590308.1f0; rtol=0.02)
+    @test isapprox(features["shape3d_mesh_volume"], 5.9246475e6; rtol=0.015)
+    @test isapprox(features["shape3d_surface_volume_ratio"], 0.09963599f0; rtol=0.02)
+    @test isapprox(features["shape3d_sphericity"], 0.26823393f0; rtol=0.02)
+    @test isapprox(features["shape3d_major_axis_length"], 314.70545f0; atol=0.01)
+    @test isapprox(features["shape3d_minor_axis_length"], 240.64522f0; atol=0.01)
+    @test isapprox(features["shape3d_least_axis_length"], 180.21617f0; atol=0.01)
+    @test isapprox(features["shape3d_elongation"], 0.7646681f0; atol=0.01)
+    @test isapprox(features["shape3d_flatness"], 0.5726503f0; atol=0.01)
+    @test isapprox(features["shape3d_voxel_volume"], 5.886506e6; atol=0.01)
+end
